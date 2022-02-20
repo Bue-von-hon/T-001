01d8cfdb025797fced02b92181cece8d4c06eff9
Bue-von-hon <dkssudvn2@gmail.com>
2022-02-20 10:59:28 +0900

    init

---
+++ src/main/java/com/kim/ProjectT001Application.java
@@ -0,0 +1,13 @@
+package com.kim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectT001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectT001Application.class, args);
+    }
+
+}
