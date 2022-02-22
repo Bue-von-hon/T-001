96e137526a2708a01784333c624eff66d3432fb5
Bue-von-hon <dkssudvn2@gmail.com>
2022-02-22 14:50:43 +0900

    feature sec config

---
+++ src/main/java/com/kim/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.kim.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/", "swagger-ui/")
+                .permitAll();
+    }
+}
