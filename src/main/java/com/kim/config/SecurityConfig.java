3b40281689d8637846ede9e3e5dc81a6d45db552
Bue-von-hon <dkssudvn2@gmail.com>
2022-02-25 14:05:56 +0900

    add form login filter

--- src/main/java/com/kim/config/SecurityConfig.java
+++ src/main/java/com/kim/config/SecurityConfig.java
@@ -2,17 +2,25 @@ package com.kim.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.authorizeRequests()
                 .mvcMatchers("/", "/login", "/sign-up", "/check-email-token",
                         "/email-login", "/login-by-email", "/swagger-ui")
                 .permitAll();
+
+        http.formLogin().loginPage("/login").permitAll()
+                .loginProcessingUrl("/perform_login");
+
+        http.logout().logoutSuccessUrl("/");
+
         http.csrf()
                 .requireCsrfProtectionMatcher(new CsrfRequireMatcher())
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
